e6ca5615589a7574c1a2877799507baffd1796a7
Rocco <Rocco@Divatos-MacBook-Air.local>
2023-05-22 18:51:03 -0400

    Fixed Interactive Map

--- dmt/src/components/Map.jsx
+++ dmt/src/components/Map.jsx
@@ -1,125 +1,9 @@
 
-//AIzaSyDVpa1qphmEE4s3ci-Ptib8PYTMKr-PT6U
-
-// import React, { useState, useEffect } from 'react';
-// import { GoogleMap, LoadScript, Autocomplete, Marker } from '@react-google-maps/api';
-
-// const containerStyle = {
-//   width: '1000px',
-//   height: '600px'
-// };
-
-// const defaultCenter = {
-//   lat: 38.206591,
-//   lng: -92.416691
-// };
-
-// export default function Map() {
-//   const [location, setLocation] = useState(defaultCenter);
-//   const [markerPosition, setMarkerPosition] = useState(defaultCenter);
-//   const [userLocation, setUserLocation] = useState(null);
-//   const [autocomplete, setAutocomplete] = useState(null);
-
-//   useEffect(() => {
-//     if (navigator.geolocation) {
-//       navigator.geolocation.getCurrentPosition((position) => {
-//         const userLocation = {
-//           lat: position.coords.latitude,
-//           lng: position.coords.longitude
-//         };
-//         setUserLocation(userLocation);
-//       }, () => {
-//         console.log('Error: The Geolocation service failed.');
-//       });
-//     } else {
-//       console.log('Error: Your browser doesn\'t support geolocation.');
-//     }
-//   }, []);
-
-//   useEffect(() => {
-//     if (userLocation) {
-//       setLocation(userLocation);
-//       setMarkerPosition(userLocation);
-//     }
-//   }, [userLocation]);
-
-//   const handleSearch = () => {
-//     if (autocomplete !== null) {
-//       const place = autocomplete.getPlace();
-//       if (place.geometry) {
-//         setLocation({
-//           lat: place.geometry.location.lat(),
-//           lng: place.geometry.location.lng(),
-//         });
-//         setMarkerPosition({
-//           lat: place.geometry.location.lat(),
-//           lng: place.geometry.location.lng(),
-//         });
-//       }
-//     }
-//   };
-
-//   const handlePlaceSelect = (place) => {
-//     if (place.geometry) {
-//       setLocation({
-//         lat: place.geometry.location.lat(),
-//         lng: place.geometry.location.lng(),
-//       });
-//       setMarkerPosition({
-//         lat: place.geometry.location.lat(),
-//         lng: place.geometry.location.lng(),
-//       });
-//     }
-//   };
-
-//   return (
-//     <LoadScript
-//       googleMapsApiKey={"AIzaSyDVpa1qphmEE4s3ci-Ptib8PYTMKr-PT6U"}
-//       libraries={['places']}
-//     >
-//       <div>
-//         <Autocomplete
-//           onLoad={(autocomplete) => setAutocomplete(autocomplete)}
-//           onPlaceChanged={() => handlePlaceSelect(autocomplete.getPlace())}
-//         >
-//           <input
-//             type="text"
-//             placeholder="Search address"
-//             style={{
-//               boxSizing: `border-box`,
-//               border: `1px solid transparent`,
-//               width: `240px`,
-//               height: `32px`,
-//               padding: `0 12px`,
-//               borderRadius: `3px`,
-//               boxShadow: `0 2px 6px rgba(0, 0, 0, 0.3)`,
-//               fontSize: `14px`,
-//               outline: `none`,
-//               textOverflow: `ellipses`,
-//               position: "absolute",
-//               left: "50%",
-//               marginLeft: "-120px",
-//             }}
-//           />
-//         </Autocomplete>
-//         <button onClick={() => handleSearch()}>Search</button>
-//       </div>
-      
-//       <GoogleMap
-//         mapContainerStyle={containerStyle}
-//         center={location}
-//         zoom={7}
-//       >
-//         {userLocation && <Marker position={userLocation} />}
-//         <Marker position={markerPosition} />
-//       </GoogleMap>
-//     </LoadScript>
-//   );
-// }
-
-
 import React, { useState, useEffect, useRef } from 'react';
-import { GoogleMap, LoadScript, Autocomplete, Marker } from '@react-google-maps/api';
+import { GoogleMap, LoadScript, Autocomplete, Marker, InfoWindow } from '@react-google-maps/api';
+import env from './env';
+
+const apiKey = env.GOOGLE_MAP_KEY;
 
 const containerStyle = {
   width: '1000px',
@@ -132,8 +16,8 @@ const defaultCenter = {
 };
 
 export default function Map() {
-  const [location, setLocation] = useState(defaultCenter);
-  const [markerPosition, setMarkerPosition] = useState(defaultCenter);
+  const [location, setLocation] = useState(null);
+  const [markers, setMarkers] = useState([]);
   const [userLocation, setUserLocation] = useState(null);
   const autocompleteRef = useRef(null);
 
@@ -145,6 +29,7 @@ export default function Map() {
           lng: position.coords.longitude
         };
         setUserLocation(userLocation);
+        setLocation(userLocation);
       }, () => {
         console.log('Error: The Geolocation service failed.');
       });
@@ -152,13 +37,6 @@ export default function Map() {
       console.log('Error: Your browser doesn\'t support geolocation.');
     }
   }, []);
-
-  useEffect(() => {
-    if (userLocation) {
-      setLocation(userLocation);
-      setMarkerPosition(userLocation);
-    }
-  }, [userLocation]);
 
   const handlePlaceSelect = () => {
     const place = autocompleteRef.current.getPlace();
@@ -168,13 +46,13 @@ export default function Map() {
         lng: place.geometry.location.lng()
       };
       setLocation(selectedLocation);
-      setMarkerPosition(selectedLocation);
+      setMarkers([{ position: selectedLocation, name: place.name }, { position: userLocation, name: 'Your Location' }]);
     }
   };
 
   return (
     <LoadScript
-      googleMapsApiKey={"AIzaSyDVpa1qphmEE4s3ci-Ptib8PYTMKr-PT6U"}
+      googleMapsApiKey={apiKey}
       libraries={['places']}
     >
       <div>
@@ -205,14 +83,68 @@ export default function Map() {
           />
         </Autocomplete>
       </div>
-      
+
       <GoogleMap
         mapContainerStyle={containerStyle}
-        center={location}
+        center={location || userLocation || defaultCenter}
         zoom={7}
       >
-        {userLocation && <Marker position={userLocation} />}
-        <Marker position={markerPosition} />
+        {userLocation && (
+          <Marker
+            position={userLocation}
+            icon={{
+              url: 'https://maps.google.com/mapfiles/ms/icons/red-dot.png',
+              labelOrigin: { x: 12, y: -15 },
+            }}
+            label={{
+              text: 'Your Location',
+              color: '#000000',
+              fontWeight: 'bold',
+              fontSize: '14px',
+              fontFamily: 'Arial',
+            }}
+          />
+        )}
+        {markers.map((marker, index) => (
+          <Marker
+            key={index}
+            position={marker.position}
+            icon={{
+              url: 'https://maps.google.com/mapfiles/ms/icons/blue-dot.png',
+              labelOrigin: { x: 12, y: -15 },
+            }}
+            label={{
+              text: marker.name,
+              color: '#000000',
+              fontWeight: 'bold',
+              fontSize: '14px',
+              fontFamily: 'Arial',
+            }}
+          />
+        ))}
+        {markers.map((marker, index) => (
+          <InfoWindow
+            key={index}
+            position={marker.position}
+            options={{
+              pixelOffset: new window.google.maps.Size(0, -30),
+            }}
+          >
+            <div
+              style={{
+                background: '#ffffff',
+                padding: '8px',
+                borderRadius: '4px',
+                color: '#000000',
+                fontWeight: 'bold',
+                fontSize: '14px',
+                fontFamily: 'Arial',
+              }}
+            >
+              {marker.name}
+            </div>
+          </InfoWindow>
+        ))}
       </GoogleMap>
     </LoadScript>
   );
--- dmt/src/trailApiPull.js
+++ dmt/src/trailApiPull.js
@@ -1,16 +1,18 @@
-const data = null;
+const fetch = require('node-fetch');
 
-const xhr = new XMLHttpRequest();
-xhr.withCredentials = true;
+const url = 'https://trailapi-trailapi.p.rapidapi.com/trails/explore/?lat=38.206591&lon=-92.416691&per_page=1000&radius=100';
+const options = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': 'c5657e40e6msh2b21adccd4a33fap1b4602jsnbcc74fccdc2f',
+    'X-RapidAPI-Host': 'trailapi-trailapi.p.rapidapi.com'
+  }
+};
 
-xhr.addEventListener('readystatechange', function () {
-	if (this.readyState === this.DONE) {
-		console.log(this.responseText);
-	}
-});
-
-xhr.open('GET', 'https://trailapi-trailapi.p.rapidapi.com/trails/explore/?lat=38.206591&lon=-92.416691&per_page=1000&radius=100');
-xhr.setRequestHeader('X-RapidAPI-Key', 'c5657e40e6msh2b21adccd4a33fap1b4602jsnbcc74fccdc2f');
-xhr.setRequestHeader('X-RapidAPI-Host', 'trailapi-trailapi.p.rapidapi.com');
-
-xhr.send(data);
+try {
+  const response = await fetch(url, options);
+  const result = await response.text();
+  console.log(result);
+} catch (error) {
+  console.error(error);
+}
